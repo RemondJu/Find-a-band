c706a287601571723f946595247f986cba06c38c
RemondJu <remond.neiluj@gmail.com>
2019-01-01 19:19:06 +0100

    contact form modified from redux to local state

--- front/src/containers/ContactFormPage.jsx
+++ front/src/containers/ContactFormPage.jsx
@@ -1,31 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { nameInputAction, emailInputAction, messageInputAction } from '../actions';
 import './ContactFormPage.scss';
 
-const ContactFormPage = (props) => {
-  const { nameInput, nameInputAction, emailInput, emailInputAction, messageInput, messageInputAction } = props;
-  return (
-    <div className="ContactFormPage">
-    <form>
-      <label htmlFor="name">
-        Name:
-      </label>
-      <input type="text" name="name" id="name" value={nameInput} onChange={(e) => nameInputAction(e.target.value)} />
-      <label htmlFor="email">
-        Email:
-      </label>
-      <input type="email" name="email" value={emailInput} onChange={(e) => emailInputAction(e.target.value)} />
-      <label htmlFor="message">
-        Message:
-      </label>
-      <textarea type="text" name="message" value={messageInput} onChange={(e) => messageInputAction(e.target.value)} />
-      <button type="submit">Send</button>
-    </form>
-  </div>
-  )
-};
+class ContactFormPage extends Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+    }
+    this.handleContactInput = this.handleContactInput.bind(this);
+    this.handleContactSubmit = this.handleContactSubmit.bind(this);
+  };
+
+  handleContactInput(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  handleContactSubmit(e) {
+    e.preventDefault();
+    console.log(this.state)
+    this.setState({
+      name: '',
+      email: '',
+      message: '',
+    })
+  }
+
+  render() {
+  const { name, email, message } = this.state;
+    return (
+      <div className="ContactFormPage">
+      <form onSubmit={this.handleContactSubmit}>
+        <label htmlFor="name">
+          Name:
+        </label>
+        <input type="text" name="name" id="name" value={name} onChange={this.handleContactInput} />
+        <label htmlFor="email">
+          Email:
+        </label>
+        <input type="email" name="email" value={email} onChange={this.handleContactInput} />
+        <label htmlFor="message">
+          Message:
+        </label>
+        <textarea type="text" name="message" value={message} onChange={this.handleContactInput} />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+    )  
+  };
+}
 
 function mstp(state) {
   return {
