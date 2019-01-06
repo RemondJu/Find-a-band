d89c000d4e14b0fb9aff99ce0282aa315228a5f8
RemondJu <remond.neiluj@gmail.com>
2019-01-06 19:11:28 +0100

    bands fetched, still need to separate band fetch from user fetch

--- front/src/containers/BandsDisplay.jsx
+++ front/src/containers/BandsDisplay.jsx
@@ -1,35 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import './BandsDisplay.scss';
 import BandCard from '../components/SearchPage/BandCard';
+import { fetchData } from '../actions/fetch';
 
-const BandsDisplay = (props) => {
-  const { bands, shouldDisplayBands } = props;
-  return (
-    <div className={shouldDisplayBands ? 'BandsDisplay' : 'hidden'}>
-      <h2>
-        Bands
-      </h2>
-      <div className="bandsCards">
-        {bands.map(band => (
-          <BandCard
-            key={band.id}
-            name={band.name}
-            members={band.members}
-            maxMembers={band.maxMembers}
-            email={band.email}
-          />
-        ))}
+class BandsDisplay extends Component {
+  componentDidMount() {
+    const { fetchDataAction } = this.props;
+    fetchDataAction('http://localhost:3600/api/bands');
+  }
+
+  render() {
+    const { bands, shouldDisplayBands } = this.props;
+    return (
+      <div className={shouldDisplayBands ? 'BandsDisplay' : 'hidden'}>
+        <h2>
+          Bands
+        </h2>
+        <div className="bandsCards">
+          {bands.map(band => (
+            <BandCard
+              key={band.band_id}
+              name={band.name}
+              members={band.members}
+              maxMembers={band.maxMembers}
+              email={band.email}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  );
-};
-
-function mstp(state) {
-  return {
-    bands: state.fetchDataSuccess,
-    shouldDisplayBands: state.shouldDisplayBands,
-  };
+    );
+  }
 }
 
-export default connect(mstp)(BandsDisplay);
+const mstp = state => ({
+  bands: state.fetchDataSuccess,
+  shouldDisplayBands: state.shouldDisplayBands,
+});
+
+const mdtp = dispatch => bindActionCreators({
+  fetchDataAction: fetchData,
+}, dispatch);
+
+export default connect(mstp, mdtp)(BandsDisplay);
