015227dedda707dea4f254d3007bbfb7f501c49b
RemondJu <remond.neiluj@gmail.com>
2019-01-20 20:17:21 +0100

    preparing search filter fetch in redux

--- front/src/actions/fetch.js
+++ front/src/actions/fetch.js
@@ -1,83 +1,83 @@
-export function itemsHasErrored(bool) {
-  return {
-    type: 'ITEM_HAS_ERRORED',
-    hasErrored: bool,
-  };
-}
-export function itemsIsLoading(bool) {
-  return {
-    type: 'ITEM_IS_LOADING',
-    isLoading: bool,
-  };
-}
-export function fetchBandsSuccess(bands) {
-  return {
-    type: 'FETCH_BANDS_SUCCESS',
-    bands,
-  };
-}
+export const itemsHasErrored = bool => ({
+  type: 'ITEM_HAS_ERRORED',
+  hasErrored: bool,
+});
 
-export function fetchBands(url) {
-  return (dispatch) => {
-    dispatch(itemsIsLoading(true));
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        dispatch(itemsIsLoading(false));
-        return response;
-      })
-      .then(response => response.json())
-      .then(items => dispatch(fetchBandsSuccess(items)))
-      .catch(() => dispatch(itemsHasErrored(true)));
-  };
-}
+export const itemsIsLoading = bool => ({
+  type: 'ITEM_IS_LOADING',
+  isLoading: bool,
+});
 
-export function fetchUsersSuccess(users) {
-  return {
-    type: 'FETCH_USERS_SUCCESS',
-    users,
-  };
-}
+export const fetchBandsSuccess = bands => ({
+  type: 'FETCH_BANDS_SUCCESS',
+  bands,
+});
 
-export function fetchUsers(url) {
-  return (dispatch) => {
-    dispatch(itemsIsLoading(true));
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        dispatch(itemsIsLoading(false));
-        return response;
-      })
-      .then(response => response.json())
-      .then(items => dispatch(fetchUsersSuccess(items)))
-      .catch(() => dispatch(itemsHasErrored(true)));
-  };
-}
+export const fetchBands = url => (dispatch) => {
+  dispatch(itemsIsLoading(true));
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      dispatch(itemsIsLoading(false));
+      return response;
+    })
+    .then(response => response.json())
+    .then(items => dispatch(fetchBandsSuccess(items)))
+    .catch(() => dispatch(itemsHasErrored(true)));
+};
 
-export function fetchBandSuccess(band) {
-  return {
-    type: 'FETCH_BAND_SUCCESS',
-    band,
-  };
-}
+export const fetchUsersSuccess = users => ({
+  type: 'FETCH_USERS_SUCCESS',
+  users,
+});
 
-export function fetchBand(url) {
-  return (dispatch) => {
-    dispatch(itemsIsLoading(true));
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        dispatch(itemsIsLoading(false));
-        return response;
-      })
-      .then(response => response.json())
-      .then(items => dispatch(fetchBandSuccess(items[0])))
-      .catch(() => dispatch(itemsHasErrored(true)));
-  };
-}
+export const fetchUsers = url => (dispatch) => {
+  dispatch(itemsIsLoading(true));
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      dispatch(itemsIsLoading(false));
+      return response;
+    })
+    .then(response => response.json())
+    .then(items => dispatch(fetchUsersSuccess(items)))
+    .catch(() => dispatch(itemsHasErrored(true)));
+};
+
+export const fetchBandSuccess = band => ({
+  type: 'FETCH_BAND_SUCCESS',
+  band,
+});
+
+export const fetchBand = url => (dispatch) => {
+  dispatch(itemsIsLoading(true));
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      dispatch(itemsIsLoading(false));
+      return response;
+    })
+    .then(response => response.json())
+    .then(items => dispatch(fetchBandSuccess(items[0])))
+    .catch(() => dispatch(itemsHasErrored(true)));
+};
+
+export const fetchSearchFilterSuccess = items => ({
+  type: 'FETCH_SEARCH_FILTER_SUCCESS',
+  items,
+});
+
+export const fetchSearchFilter = (searchFilter, url) => (dispatch) => {
+  dispatch(itemsIsLoading(true));
+  fetch(url)
+    .then(res => res.json())
+    .then(items => dispatch(fetchSearchFilterSuccess(items)))
+    .then(() => dispatch(itemsIsLoading(false)))
+    .catch(() => dispatch(itemsHasErrored(true)));
+};
--- front/src/components/SearchBar.jsx
+++ front/src/components/SearchBar.jsx
@@ -5,20 +5,31 @@ import { NavLink, withRouter } from 'react-router-dom';
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      searchFilter: '',
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      searchFilter: e.target.value,
+    });
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    console.log('will do stuff');
+    const { searchFilter } = this.state;
+    console.log('will fetch stuff');
   }
 
   render() {
+    const { searchFilter } = this.state;
     return (
       <div className="SearchBar">
         <form onSubmit={this.handleSubmit}>
-          <input type="text" />
+          <input type="text" onChange={this.handleChange} value={searchFilter} />
           <NavLink to="/search">
             <button type="submit">
               <img src="https://image.flaticon.com/icons/png/512/9/9383.png" alt="search button" />
